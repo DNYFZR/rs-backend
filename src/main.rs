c8e7ca00d98bf37e743f7bf1a5af17e5f5695948
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-10-27 18:43:23 +0000

    Added initial API implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+// Application Backend 
+mod api;
+mod database;
+
+use serde_json;
+use std::fs;
+
+#[tokio::main]
+async fn main() {
+    let url = "https://api.github.com/search/repositories?q=rust&sort=stars&order=desc&per_page=10&page=1";
+    let raw_data = api::get_data(url).await.expect("failed to call API");
+    
+    // Test write out
+    let json_data = serde_json::to_string_pretty(&raw_data).expect("failed to parse json");
+    fs::write("database/test.json", json_data).expect("failed to write json file");
+
+
+   println!("{:#?}", raw_data);
 }
+
+
