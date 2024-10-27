fcc3f333f09f53024d981fd3a5780796a60be670
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-10-27 21:07:17 +0000

    Added input handling

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,33 @@ use std::fs;
 
 #[tokio::main]
 async fn main() {
-    let url = "https://api.github.com/search/repositories?q=rust&sort=stars&order=desc&per_page=10&page=1";
-    let raw_data = api::get_data(url).await.expect("failed to call API");
+    // Program input
+    let user_search:Vec<_> = std::env::args().collect();
+
+    if user_search.len() > 1 {
+        println!("Searching for : {:#?}", user_search[1]);
+
+        // Process API call
+        let raw_data = api::get(&user_search[1])
+            .await
+            .expect("failed to call API");
+        
+        let json_data = serde_json::to_string_pretty(&raw_data)
+            .expect("failed to parse json");
+
+        // Write out    
+        fs::write(
+            format!("database/{}.json", user_search[1].replace(" ", "-")),
+            json_data
+        ).expect("failed to write json file");
+        
+        println!("Output written to : {}.json", user_search[1].replace(" ", "-"));
+        println!("|--- SUCCESS ---|");
+    } 
     
-    // Test write out
-    let json_data = serde_json::to_string_pretty(&raw_data).expect("failed to parse json");
-    fs::write("database/test.json", json_data).expect("failed to write json file");
-
-
-   println!("{:#?}", raw_data);
+    else {
+        println!("Please provide a search term, using quotes if whitespace is required")
+    }    
 }
 
 
