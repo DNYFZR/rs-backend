9df2fa5a2add931c1cfe5d4c393edde18f7d525c
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-10-28 21:07:47 +0000

    Created db manager

--- src/database.rs
+++ src/database.rs
@@ -1,23 +1,55 @@
-// // Backend Database Manager
-use rusqlite::Connection;
-use std::path;
+// Backend Data Storage Controller
+use crate::structs::Repository;
 
-fn connect(database:Option<&str>) -> Connection {
-  match database {
-    Some(database) => {
-      match  path::Path::new(database).exists() {
-          true => return Connection::open(database).unwrap(),
-          false => return Connection::open(":memory:").unwrap(),
-      }
-    },
-    
-    None => return Connection::open(":memory:").unwrap(),
-  }
+use std::fs::write;
+use rusqlite::{ Connection, Result };
+use serde_json;
+
+pub fn update(table:&str, data:Vec<Repository>) -> Result<(), serde_json::Error> {
+    // Consts
+    const DB: &str = "database/app.db"; 
+    const CREATE_TABLE_QUERY: &str = "CREATE TABLE IF NOT EXISTS api_data (
+            name TEXT PRIMARY KEY,
+            data JSON
+        )";
+
+    // Open the database, falling back to in-memory if path is not usable
+    let conn = match Connection::open(DB) {
+        Ok(conn) => conn,
+        Err(_) => Connection::open_in_memory().expect("failed to open in-memory DB"),
+    };
+
+    // Create the api_data table if it does not exist
+    conn.execute(
+        CREATE_TABLE_QUERY,
+        [],
+    ).expect("failed to create table");
+
+    // Insert or update the data in the api_data table
+    match serde_json::to_string(&data) {
+        Ok(res) => {
+            conn.execute(
+                "INSERT OR REPLACE INTO api_data (name, data) VALUES (?1, ?2)",
+                (table, res),
+            ).expect(&format!("failed to update {}", table));
+            return Ok(());
+        },
+
+        Err(e) => {
+            println!("error : {e}");
+            return Err(e);
+        },
+     }
 }
 
-pub fn query(database:Option<&str>, query: &str) -> usize {
-  let conn = connect(database);
-  let exe = conn.execute(query, []);
-
-  return exe.unwrap()
+fn write_json(table: &str, data: Vec<Repository>) {
+    let filename = table.replace(" ", "-");
+    let json_data = serde_json::to_string_pretty(&data)
+        .expect("failed to parse json");
+    
+    write(format!("database/{}.json", filename), json_data
+        ).expect("failed to write json file");
+    
+    println!("Data written to : {}.json", filename);
+        
 }
