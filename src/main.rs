7bab9ce7f55e8b64e64f9b9249df8ac0f26466bf
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-10-29 11:01:11 +0000

    Implement CI/CD  (#3)
    * Added module testing
    * Created json utils (later use)
    * Updated main implementation
    * Created test data file
    * Created deployment script

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,19 @@ async fn main() {
     let user_search:Vec<_> = std::env::args().collect();
 
     if user_search.len() > 1 {
+        let table = "api_data";
         let search_term = user_search[1].clone();
-        let table_name = search_term.replace(" ", "-");
+        let name = search_term.replace(" ", "-");
         println!("Searching for : {:#?}", search_term);
 
         // Process API call
-        let api_data = api::get(&search_term)
+        let data = api::get(&search_term)
             .await
             .expect("failed to call API");
-
+        
         // Update DB
-        match database::update(&table_name, api_data) {
-            Ok(_) => println!("DB table {table_name} updated"),
+        match database::update(&table, &name, data) {
+            Ok(_) => println!("DB table {name} updated"),
             Err(e) => println!("Error : {e}"),
         }
 
